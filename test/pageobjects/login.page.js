1007c04b17f5212dde6d75107882c2f9a8e71265
Mykhailo Sheptun <michael.sheptun@gmail.com>
2021-03-25 22:33:19 -0400

    Created test cases "Password" and "Email" error appeared login page added

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -7,6 +7,8 @@ class LoginPage extends Page {
     get inputPassword () { return $('#normal_login_password'); }
     get buttonSubmit () { return $('.login-form-button'); }
     get errorToast(){return $('.ant-notification-notice-message')}
+    get errorMessage(){return $('.ant-form-item-explain-error');}
+
 
     open () {
         return super.open('/user/login');
@@ -29,6 +31,24 @@ class LoginPage extends Page {
 
     errorToastAppeared(){
         expect(this.errorToast).toBeDisplayed();
+    }
+
+    errorMessageAppeared(){
+        expect(this.errorMessage === 'Required');
+    }
+
+    errorEmailMessage(){
+        expect(this.errorMessage === '\'email\' is not a valid email')
+    }
+
+
+
+    clearLoging(){
+        this.inputUsername.clearValue();
+    }
+
+    clearPass(){
+        this.inputPassword.clearValue();
     }
 }
 
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -10,7 +10,7 @@ describe('Auth', () => {
         browser.execute('window.localStorage.clear()')
     })
 
-    it('user logs in with valid data', () => {
+    it('user log in with valid data', () => {
         LoginPage.setLogin('yecomel462@grokleft.com');
         LoginPage.setPassword('123123123');
         LoginPage.clickSubmitButton();
@@ -20,15 +20,36 @@ describe('Auth', () => {
 
     it('Submit button is disabled if login and password are absent', ()=>{
 
-
         LoginPage.submitButtonIsDisabled()
     });
+
     it('fails if invalid data provided', ()=>{
       LoginPage.setLogin('example@example.com');
       LoginPage.setPassword('123123');
       LoginPage.clickSubmitButton();
       LoginPage.errorToastAppeared();
     })
+
+    it('"*Required" alert appear  ', () => {
+        LoginPage.setLogin('yecomel462@grokleft.com');
+        LoginPage.clearLoging()
+        LoginPage.errorMessageAppeared();
+
+    });
+
+    it('"*Email" alert appear  ', () => {
+        LoginPage.setLogin('yecomel462@grokleft.com');
+        LoginPage.clearLoging()
+        LoginPage.errorEmailMessage();
+
+    });
+
+    it('"*Email" alert appear  ', () => {
+        LoginPage.setPassword('123123');
+        LoginPage.clearPass();
+        LoginPage.errorMessageAppeared();
+
+    });
 
 });
 
